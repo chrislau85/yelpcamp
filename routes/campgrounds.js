9e21a3cf428a6312f57398b7f9c58c33854bbcea
clau85 <chrisv208@gmail.com>
2016-03-19 17:18:40 +0000

    add ability for users to delete campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -77,8 +77,16 @@ router.put("/:id", function(req, res){
     
 });
 
-// UPDATE CAMPGROUND ROUTE
-
+// DESTROY CAMPGROUND ROUTE
+router.delete("/:id", function(req, res){
+   Campground.findByIdAndRemove(req.params.id, function(err){
+       if(err){
+           res.redirect("/campgrounds");
+       }else{
+           res.redirect("/campgrounds");
+       }
+   });
+});
 
 //middleware
 function isLoggedIn(req, res, next){
